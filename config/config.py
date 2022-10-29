726d8b0cfdcf29740418b8d6e51e08b987e88a18
Aryanve595 <90677181+Aryanve595@users.noreply.github.com>
2022-10-29 20:56:49 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", "https://t.me/myworldGJ516")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/GJ516_DISCUSS_GROUP")
+    "SUPPORT_GROUP", "https://t.me/+p2A5gHTe9_YzNDk1")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
